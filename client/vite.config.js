9d50155d2381957479496ff47d054f92d6008a32
Neil katira and Jayant Dehspande <rhujuta.ingale@gmail.com>
2026-09-13 10:52:45 -0700

    Deploy client to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? "/EverClear-Care/" : "/",
   server: { port: 5173 },
 });
